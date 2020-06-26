4f13f5b4988dd004c99a36078aa2c3845b59ce2d
Yang Lei <979086588@qq.com>
2020-06-26 12:55:23 +0800

    update go modules

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/fansyang/hello
 
 go 1.14
 
-require github.com/google/go-cmp v0.4.0
+require (
+	github.com/google/go-cmp v0.4.0
+	github.com/google/uuid v1.1.1 // indirect
+)
--- hello.go
+++ hello.go
@@ -5,9 +5,13 @@ import (
 
 	"github.com/fansyang/hello/morestrings"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/uuid"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+
+	id := uuid.New().String()
+	fmt.Println("UUID:", id)
 }
