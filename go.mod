d2e8451defb81df98aca6260c34603712ff24b73
Yang Lei <979086588@qq.com>
2020-07-12 09:21:02 +0800

    add go-pinyin module

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/google/go-cmp v0.4.0
 	github.com/google/uuid v1.1.1
+	github.com/mozillazg/go-pinyin v0.18.0
 )
--- hello.go
+++ hello.go
@@ -6,6 +6,7 @@ import (
 	"github.com/fansyang/hello/morestrings"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/uuid"
+	pinyin "github.com/mozillazg/go-pinyin"
 )
 
 func main() {
@@ -14,4 +15,18 @@ func main() {
 
 	id := uuid.New().String()
 	fmt.Println("UUID:", id)
+
+	hans := "中国人"
+
+	// 默认
+	a := pinyin.NewArgs()
+	fmt.Println(pinyin.Pinyin(hans, a))
+
+	// 包含声调
+	a.Style = pinyin.Tone
+	fmt.Println(pinyin.Pinyin(hans, a))
+
+	// 声调用数字表示
+	a.Style = pinyin.Tone2
+	fmt.Println(pinyin.Pinyin(hans, a))
 }
