cf2a22dd211bf9573ad9565b06ae93373102f795
Yang Lei <979086588@qq.com>
2020-07-22 12:48:28 +0800

    add go-pinyin module examples

--- hello.go
+++ hello.go
@@ -21,8 +21,15 @@ func main() {
 	// 默认
 	a := pinyin.NewArgs()
 	fmt.Println("default:", pinyin.Pinyin(hans, a)) // [[zhong] [guo] [ren]]
-	fmt.Println(pinyin.Slug(hans, a))               // zhong-guo-ren
+	// Convert 跟 Pinyin 的唯一区别就是 a 参数可以是 nil
+	fmt.Println("pinyin.Convert():", pinyin.Convert(hans, nil))
+	// LazyPinyin 汉字转拼音，与 `Pinyin` 的区别是： 返回值类型不同，并且不支持多音字模式，每个汉字只取第一个音
+	// LazyConvert 跟 LazyPinyin 的唯一区别就是 a 参数可以是 nil
+	fmt.Println(pinyin.LazyPinyin(hans, a)) // [zhong guo ren]
+	// Separator 默认配置： `Slug` 中 Join 所用的分隔符，默认为"-"
+	fmt.Println(pinyin.Slug(hans, a)) // zhong-guo-ren
 
+	// Style 默认配置：风格
 	a.Style = pinyin.Finals
 	fmt.Println(pinyin.Pinyin(hans, a)) // [[ong] [uo] [en]]
 
@@ -55,7 +62,7 @@ func main() {
 	a.Style = pinyin.Tone3
 	fmt.Println(pinyin.Pinyin(hans, a)) // [[zhong1] [guo2] [ren2]]
 
-	// 开启多音字模式
+	// Heteronym 默认配置：是否启用多音字模式
 	b := pinyin.NewArgs()
 	b.Heteronym = true
 	fmt.Println(pinyin.Pinyin(hans, b)) // [[zhong zhong] [guo] [ren]]
